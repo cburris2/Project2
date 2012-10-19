639bd5f20e73b6bfd9bfb77001f66d9775cc847e
Charlie <charlie@debianBox.rbh.rockynet.wireless.unca>
2012-10-19 00:02:16 -0400

    Started insert method

--- src/TwoDArray.cpp
+++ src/TwoDArray.cpp
@@ -1,43 +1,56 @@
 #include <Node.h>
+#include <TwoDArray.h>
 #include <iostream>
+#include <string>
 
 
-<template typename>
+<template typename T>
 TwoDArray(int r, int c, int def){
+ r = 10;
+ c = 10;
+ def = 0;
+ int** array = new int*[r][c];
+ 	for (int i =0; i< 10; i++){
+	  array[i] = new int[10]; 
+	 
+	}
 
 
 }
 
-<template typename>
+<template typename T>
 ~TwoDArray(){};
 
 
-<template typename>
+<template typename T> 
 void insert(int r, int c, T value){
-
-
+  asser(!array.empty());
+  int k = getNumRows();
+  int x = getNumCols();
+    array[k+1][x+1] = value;	
+    
 } 
 
-<template typename>
+<template typename T>
 T access(int r, int c){
 
 
 }
 
-<template typename>
+<template typename T>
 void remove (int r, int c){
 
 
 }
 
-<template typename>
+<template typename T>
 int getNumRows() {
 
   return 0;
 
 }
 
-<template typename>
+<template typename T>
 int getNumCols(){
 
   return 0;
